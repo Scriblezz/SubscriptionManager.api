5b02032d21428c8b90ae78c3adf7436cc5870638
Brady Parksion <bparksion23@wou.edu>
2026-03-19 21:38:21 -0700

    feat: add User entity and database setup
    - Created User entity with Email, PasswordHash, CreatedAt
    - Added Users DbSet to AppDbContext
    - Configured unique index on User.Email
    - Created and applied EF Core migration (AddUserEntity)

--- SubscriptionManager.Api/Data/AppDbContext.cs
+++ SubscriptionManager.Api/Data/AppDbContext.cs
@@ -8,5 +8,16 @@ namespace SubscriptionManager.Api.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Subscription> Subscriptions { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(s => s.Email)
+                .IsUnique();
+        }
     }
 }
