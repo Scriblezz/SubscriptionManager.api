7a15465c475ec0d08da3d335d155a69d279911e2
Brady Parksion <bparksion23@wou.edu>
2026-04-14 19:17:41 -0700

    Add Subscriptions page with API integration

--- SubscriptionManager.client/src/pages/Subscriptions.jsx
+++ SubscriptionManager.client/src/pages/Subscriptions.jsx
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react"
 function Subscriptions() {
+  const [subscriptions, setSubscriptions] = useState([]);
+  async function getSubscriptions() {
+    const token = localStorage.getItem("token");
+    const response = await fetch('http://localhost:5001/api/Subscriptions', {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch subscriptions");
+    }
+
+    const data = await response.json();
+    setSubscriptions(data.items);
+  }
+
+  useEffect(() => {
+    getSubscriptions();
+  }, [])
+
   return (
     <div>
       <h1>Subscriptions</h1>
+      <ul>
+        {subscriptions.map((sub) => (
+          <li key={sub.id}>Name: {sub.name}, Price: ${sub.price}, Category: {sub.category}, Billing Cycle: {sub.billingCycle}</li>
+        ))}
+      </ul>
     </div>
   )
 }
