b3c6cd632254835e58e02a76bebb8fe5a6c4ad31
Scriblez <hawtblooded@hotmail.com>
2026-04-26 21:01:52 -0700

    Add protected routes, home page redirect, and persistent dark mode

--- SubscriptionManager.client/src/App.jsx
+++ SubscriptionManager.client/src/App.jsx
@@ -1,25 +1,43 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Subscriptions from './pages/Subscriptions'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
-  const [isDark, setIsDark] = useState(false)
+  const [isDark, setIsDark] = useState(() => {
+    return localStorage.getItem('darkMode') === 'true'
+  })
 
   function toggleDark() {
     document.documentElement.classList.toggle('dark')
-    setIsDark(prev => !prev)
+    setIsDark(prev => {
+      localStorage.setItem('darkMode', !prev)
+      return !prev
+    })
   }
+
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.classList.add('dark')
+    }
+  }, [])
+
   return (
     <div className={isDark ? 'dark' : ''}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login toggleDark={toggleDark} isDark={isDark} />} />
-        <Route path="/register" element={<Register toggleDark={toggleDark} isDark={isDark} />} />
-        <Route path="/subscriptions" element={<Subscriptions toggleDark={toggleDark} isDark={isDark}/>} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login toggleDark={toggleDark} isDark={isDark} />} />
+          <Route path="/register" element={<Register toggleDark={toggleDark} isDark={isDark} />} />
+          <Route path="/subscriptions" element={
+            <ProtectedRoute>
+              <Subscriptions toggleDark={toggleDark} isDark={isDark} />
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
